516e30427494280950d560843fb8c390da476104
PurnachandraRao <pcr.bobbepalli@gmail.com>
2026-09-10 21:22:00 +0530

    Add adaptive target definition and print statements

---
+++ 14_adaptive_target_definition.py
@@ -0,0 +1,8 @@
+E_TARGET = (
+    GROUND_ENERGY +
+    TARGET_TOLERANCE
+)
+
+print("Reference Energy:", GROUND_ENERGY)
+print("Target Tolerance:", TARGET_TOLERANCE)
+print("Adaptive Target:", E_TARGET)
