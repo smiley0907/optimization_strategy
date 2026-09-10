d6df10a9b616d33cc51d7dd7069dd29c1541556d
PurnachandraRao <pcr.bobbepalli@gmail.com>
2026-09-10 21:18:14 +0530

    Add environment information script

---
+++ 03_environment_information.py
@@ -0,0 +1,7 @@
+import sys
+import qiskit
+
+print("Python version:", sys.version)
+print("Qiskit version:", qiskit.__version__)
+print("NumPy version:", np.__version__)
+print("Pandas version:", pd.__version__)
