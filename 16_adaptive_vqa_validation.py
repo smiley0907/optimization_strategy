b45a0f811ec9047be187bc2e225c4d429264c2cf
PurnachandraRao <pcr.bobbepalli@gmail.com>
2026-09-10 21:22:48 +0530

    Add adaptive VQA validation script

---
+++ 16_adaptive_vqa_validation.py
@@ -0,0 +1,13 @@
+print("Proposed Adaptive VQA Iterations")
+
+display(
+    adaptive_df[
+        [
+            "Circuit Depth",
+            "Iterations",
+            "Final Energy",
+            "Execution Time (s)",
+            "Termination"
+        ]
+    ]
+)
