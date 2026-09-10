610faa50e6f6712424934b9ec45cece2f9d8c36e
PurnachandraRao <pcr.bobbepalli@gmail.com>
2026-09-10 21:18:36 +0530

    Create 04_reproducibility_configuration.py

---
+++ 04_reproducibility_configuration.py
@@ -0,0 +1,9 @@
+RANDOM_SEED = 42
+MAX_EVALUATIONS = 200
+TARGET_TOLERANCE = 1e-4
+
+np.random.seed(RANDOM_SEED)
+
+print("Random Seed:", RANDOM_SEED)
+print("Maximum Evaluations:", MAX_EVALUATIONS)
+print("Target Tolerance:", TARGET_TOLERANCE)
