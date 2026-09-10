10bb1f6257d20fea1e055a393f5943f1363eb2c0
PurnachandraRao <pcr.bobbepalli@gmail.com>
2026-09-10 21:21:37 +0530

    Create 13_conventional_vqa_validation.py

---
+++ 13_conventional_vqa_validation.py
@@ -0,0 +1,12 @@
+print("Conventional VQA Iterations")
+
+display(
+    conventional_df[
+        [
+            "Circuit Depth",
+            "Iterations",
+            "Final Energy",
+            "Execution Time (s)"
+        ]
+    ]
+)
